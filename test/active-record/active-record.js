59b783be146a60a9e380696eccca053918f863d5
PinkiePie <PinkaminaDianePie@gmail.com>
2015-04-15 23:31:16 +0300

    added chainable methods to active record

--- test/active-record/active-record.js
+++ test/active-record/active-record.js
@@ -8,16 +8,44 @@ describe('active-record', () => {
 	});
 
 	describe('"Base" class', () => {
-		it('should have init method', () => {
+		it('should have "init" method', () => {
 			ActiveRecord.Base.init.should.be.a('function');
 		});
-		it('init method should not be called without subclassing', () => {
-			ActiveRecord.Base.init.bind(ActiveRecord.Base).should.throw(TypeError);
-			class A extends ActiveRecord.Base{
-
-			}
-			A.init.bind(ActiveRecord.Base).should.throw(TypeError);
-			A.init.bind(A).should.not.throw(TypeError);
+		describe('"init" method', () => {
+			var A;
+			beforeEach(function(){
+				A = class A extends ActiveRecord.Base{
+					constructor(){
+						super();
+						this.id = 0;
+						this.name = "foo";
+						this.blocked = false;
+						this.tokens = [];	
+						this.position = {
+							alt: 0,
+							long: 0
+						}
+					}
+				}
+			});
+			it('should not be called without subclassing', () => {
+				ActiveRecord.Base.init.bind(ActiveRecord.Base).should.throw(TypeError);
+				A.init.bind(ActiveRecord.Base).should.throw(TypeError);
+				A.init.bind(A).should.not.throw(TypeError);
+			});
+			it('should create access methods for each atomic field in constructor', () => {
+				A.init();
+				A.getById.should.be.a('function');
+				A.getByName.should.be.a('function');
+				A.getByBlocked.should.be.a('function');
+				should.not.exist(A.getByTokens);
+				should.not.exist(A.getByPosition);
+			});
+			it('should create chainable access methods', () => {
+				A.init();
+				A.getById(123, 456).getById("asd").getById(false);
+				A.getById(654, 321).getById("qwe").getById(true);
+			});
 		});
 	});
 });
